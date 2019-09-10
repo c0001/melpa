a26d8ab89b2232991e344d47b0f8411e96572106
Entropy <bmsac0001@gmail.com>
2019-09-10 08:39:13 +0800

    Add recipe fetcher `eemacs`

--- package-build/package-recipe.el
+++ package-build/package-recipe.el
@@ -29,9 +29,16 @@
 ;;; Code:
 
 (require 'eieio)
-(defvar package-build-local-remote
-  (expand-file-name "../../../../.git/modules/elements/submodules/upstream"
+(defvar package-build-local-host
+  (expand-file-name "../../../../.git/modules/elements/submodules"
                     (file-name-directory load-file-name)))
+
+(defvar package-build-local-upstream-remote
+  (expand-file-name "upstream" package-build-local-host))
+
+(defvar package-build-local-eemacs-remote
+  (expand-file-name "eemacs-packages" package-build-local-host))
+
 (defvar package-build-recipes-dir)
 (defvar package-build-working-dir)
 
@@ -70,12 +77,16 @@
 [0-9]\\{2\\}:[0-9]\\{2\\}:[0-9]\\{2\\}\\( [+-][0-9]\\{4\\}\\)?\\)")))
 
 (defclass package-github-recipe (package-git-recipe)
-  ((url-format      :initform (concat package-build-local-remote "/%s"))
-   (repopage-format :initform (concat package-build-local-remote "/%s"))))
+  ((url-format      :initform (concat package-build-local-upstream-remote "/%s"))
+   (repopage-format :initform (concat package-build-local-upstream-remote "/%s"))))
 
 (defclass package-gitlab-recipe (package-git-recipe)
-  ((url-format      :initform (concat package-build-local-remote "/%s"))
-   (repopage-format :initform (concat package-build-local-remote "/%s"))))
+  ((url-format      :initform (concat package-build-local-upstream-remote "/%s"))
+   (repopage-format :initform (concat package-build-local-upstream-remote "/%s"))))
+
+(defclass package-eemacs-recipe (package-git-recipe)
+  ((url-format      :initform (concat package-build-local-eemacs-remote "/%s"))
+   (repopage-format :initform (concat package-build-local-eemacs-remote "/%s"))))
 
 ;;;; Mercurial
 
@@ -85,8 +96,8 @@
 [0-9]\\{2\\}:[0-9]\\{2\\}\\( [+-][0-9]\\{4\\}\\)?\\)")))
 
 (defclass package-bitbucket-recipe (package-hg-recipe)
-  ((url-format      :initform (concat package-build-local-remote "/%s"))
-   (repopage-format :initform (concat package-build-local-remote "/%s"))))
+  ((url-format      :initform (concat package-build-local-upstream-remote "/%s"))
+   (repopage-format :initform (concat package-build-local-upstream-remote "/%s"))))
 
 ;;; Interface
 
@@ -140,7 +151,7 @@ file is invalid, then raise an error."
           (cl-assert (memq thing all-keys) nil "Unknown keyword %S" thing)))
       (let ((fetcher (plist-get plist :fetcher)))
         (cl-assert fetcher nil ":fetcher is missing")
-        (if (memq fetcher '(github gitlab bitbucket))
+        (if (memq fetcher '(github gitlab bitbucket eemacs))
             (progn
               (cl-assert (plist-get plist :repo) ":repo is missing")
               (cl-assert (not (plist-get plist :url)) ":url is redundant"))

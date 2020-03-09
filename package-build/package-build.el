24b0c0324db58efbe8761e99e4a047352b75eeb0
Entropy <bmsac0001@gmail.com>
2020-03-10 05:41:13 +0800

    Remove fetch behaviour for preventing net connecting lag

--- package-build/package-build.el
+++ package-build/package-build.el
@@ -250,7 +250,8 @@ is used instead."
      ((and (file-exists-p (expand-file-name ".git" dir))
            (string-equal (package-build--used-url rcp) url))
       (package-build--message "Updating %s" dir)
-      (package-build--run-process dir nil "git" "fetch" "-f" "--all" "--tags"))
+      ;; (package-build--run-process dir nil "git" "fetch" "-f" "--all" "--tags")
+      )
      (t
       (when (file-exists-p dir)
         (delete-directory dir t))
@@ -277,7 +278,7 @@ is used instead."
 
     ;; Commented the branch reset behaviour for reset to origin/master
     ;; that eemacs-ext checkout the commit required before melpa init.
-    
+
     ;; (unless rev
     ;;   (setq rev (or (oref rcp commit)
     ;;                 (concat "origin/"
